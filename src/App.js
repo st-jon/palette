1e866d76be52cef5239ee677cfe6245da9bf3d22
christophe <c.johanny@gmail.com>
2019-05-22 19:14:54 +0200

    slider done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Palette from './components/Palette'
+
 import seedColors from './seedColors'
+import {generatePalette} from './utilities/colorHelpers'
 
 function App() {
 	return (
 		<div className="App">
-			<Palette {...seedColors[2]}/>
+			<Palette palette={generatePalette(seedColors[4])}/>
 		</div>
 	)
 }
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,14 +1,37 @@
 import React from 'react'
+import Slider from 'rc-slider'
+
 import ColorBox from './ColorBox'
+
+import 'rc-slider/assets/index.css'
 import '../styles/Palette.css'
 
+
+
 class Palette extends React.Component {
+    state = {level: 500}
+
+    changeLevel = (level) => {
+        this.setState({level})
+    }
+
     render() {
-        const colorBoxes = this.props.colors.map(color => {
-            return <ColorBox  background={color.color} name={color.name}/>
+        const {colors} = this.props.palette
+        const {level} = this.state
+        const colorBoxes = colors[level].map(color => {
+            return <ColorBox  background={color.hex} name={color.name}/>
         })
         return (
             <div className="palette">
+                <div className="slider">
+                    <Slider 
+                        defaultValue={level} 
+                        min={100} 
+                        max={900}
+                        step={100}
+                        onAfterChange={this.changeLevel}
+                    />
+                </div>
                 <div className="palette-colors">
                     {colorBoxes}
                 </div>
