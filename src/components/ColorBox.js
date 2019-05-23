8a09796b3b65397b8fe1c8599770bb3200987ce2
christophe <c.johanny@gmail.com>
2019-05-23 14:52:57 +0200

    font color reactive to luminance

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {CopyToClipboard} from 'react-copy-to-clipboard'
+import chroma from 'chroma-js'
+
 import '../styles/ColorBox.css'
 
 class ColorBox extends React.Component {
@@ -18,23 +20,26 @@ class ColorBox extends React.Component {
     render() {
         const {name, background, paletteId, id, showLink} = this.props
         const {copied} = this.state
+        const isDark = chroma(background).luminance() <= 0.08
+        const isLight = chroma(background).luminance() >= 0.8
+
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{background}} className="colorBox">
                 <div style={{background}} className={`copy-overlay ${copied && 'show'}`}></div>
                 <div className={`copy-message ${copied && 'show'}`}>
-                    <h1>copied!</h1>
-                    <div className="copy-background">{background}</div>
+                    <h1 className={isLight ? "dark-text" : undefined}>copied!</h1>
+                    <div className={isLight ? "copy-background dark-text" : "copy-background"}>{background}</div>
                 </div>
                     <div className="copy-container">
                         <div className="box-content">
-                            <span>{name}</span>
+                            <span className={isDark ? 'light-text': undefined}>{name}</span>
                         </div>
-                        <button className="copy-btn">COPY</button>
+                        <button className={isLight ? "copy-btn dark-text" : "copy-btn"}>COPY</button>
                     </div>
                     {showLink && (
                         <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()}>
-                            <span className="more">More</span>
+                            <span className={isLight ? "more dark-text" : "more"}>MORE</span>
                         </Link>
                     )}
                 </div>
